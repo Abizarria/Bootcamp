8a56cabb63f739e427087c3c4be5394f77f71e2c
Abizarria <abs4mail@gmail.com>
2022-07-21 17:03:58 -0300

    Atualização - Explorando Diretivas com Angular

---
+++ Aulas/Angular/liveAngular/src/app/app.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  count = 0;
+  nome = "Transferindo para a View";    // <h2>{{ nome }}</h2>
+  text = '';
+
+  pessoas = [
+    {
+      nome: "Adilson",
+      sobrenome: "Bizarria"
+    },
+    {
+      nome: "Katya",
+      sobrenome: "Lorente"
+    },
+    {
+      nome: "Pedro",
+      sobrenome: "Bizarria"
+    },
+    {
+      nome: "Cláudia",
+      sobrenome: "Utempergher"
+    }
+  ]
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+    console.log(this.pessoas);
+
+    let interval = setInterval(() => {
+      this.count++;
+      if (this.count === 10) {
+        clearInterval(interval);
+      }
+    }, 1000)
+  }
+
+  clicou(nome: string): void {
+    console.log('Clicou em mim', nome);
+  }
+  title = 'liveAngular';
+}
